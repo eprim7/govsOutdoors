1f7d2cd4ba1700b5bc1f5fc85fe777a1fb0c0321
bri2319 <bricenh6300@gmail.com>
2024-09-16 13:33:52 -0500

    i want this to work so bad

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
 
         <>
             <Header />
-            <h1>Testing guess who figured it out </h1>
+            <h1>Testing gugksadfglagured it out </h1>
         </>
 
     );
