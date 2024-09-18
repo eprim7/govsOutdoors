c15bb22a812e32ded8befdedc058c1734085fd0e
eprim7 <evan.primasing7@gmail.com>
2024-09-18 14:43:51 -0500

    fixed the about page

--- src/components/googleMap/GoogleMap.jsx
+++ src/components/googleMap/GoogleMap.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import styles from './GoogleMap.module.css';
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY; // Use the updated environment variable
 
 const center = {
   lat: 36.535665486542605,
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,13 @@
 import styles from "./about.module.css";
 import Header from "../../components/Header/Header";
+import GoogleMapComponent from "../../components/googleMap/GoogleMap";
 
 function About(){
     return(
         <>
         <Header />
         <h1 className={styles.h1}>we could do this like the about page and FAQ page or we could put those on separate pages idk</h1>
+        <GoogleMapComponent />
         </>
     );
 }
