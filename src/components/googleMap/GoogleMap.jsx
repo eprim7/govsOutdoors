ad12cfdd88d86f1262578232c512afb034d1d832
eprim7 <evan.primasing7@gmail.com>
2024-09-18 14:39:23 -0500

    adding map to about page

---
+++ src/components/googleMap/GoogleMap.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import styles from './GoogleMap.module.css';
+
+const apiKey = process.env.API_KEY;
+
+const center = {
+  lat: 36.535665486542605,
+  lng: -87.35412665160283
+};
+
+const position = {
+  lat: 36.535665486542605,
+  lng: -87.35412665160283
+};
+
+const GoogleMapComponent = () => (
+  <LoadScript
+    googleMapsApiKey={apiKey}
+  >
+    <GoogleMap
+      mapContainerClassName={styles.containerStyle}
+      center={center}
+      zoom={18}
+    >
+      <Marker position={position} />
+    </GoogleMap>
+  </LoadScript>
+);
+
+export default GoogleMapComponent;
---
+++ src/pages/Gear/Gear.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+
+function Gear(){
+
+    return(
+
+        <>
+            <Header />
+            <h1>Gear Page</h1>
+        </>
+    );
+}
+
+export default Gear;
